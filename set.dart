61e07d9e34cf9c5ca4983c81c1813789aaa004c7
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-07 11:56:44 +0700

    Create set.dart

---
+++ set.dart
@@ -0,0 +1,29 @@
+//INI CARA MEMBUAT SET DI DEKLARASIKAN SECARA LANGSUNG DAN JUGA MEMANIPULASI SET TERSEBUT
+// void main(){
+//   Set<int> angka = {10,20,30};
+//   var angka1 = <int>{40,80,90};
+//   final nama = <String>{'iki','iky','iqi'};
+//   var angka2 = {20,40,60};
+//   var angka3 = {
+//     20,
+//     10,
+//     40
+//   };
+
+//   print(angka);
+//   print(angka1);
+//   print(nama);
+
+//   angka.add(11);
+//   angka1.add(88);
+//   nama.add('iki');
+
+//   print(angka);
+//   print(angka1);
+//   print(nama);
+
+//   nama.remove('iqi');
+//   print(nama);
+//   print(angka2);
+//   print(angka3);
+// }
